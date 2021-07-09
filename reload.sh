6c77ef1377f37064ea99a55327743a6770bfaad7
unknownpgr <unknownpgr@gmail.com>
2021-07-09 08:57:24 +0000

    Ignore mkdir error

--- reload.sh
+++ reload.sh
@@ -1,5 +1,5 @@
 docker-compose down --remove-orphans
 docker-compose up -d --build
 docker container ls
-mkdir /srv/server-logging/docker-compose
+mkdir /srv/server-logging/docker-compose > /dev/null
 docker-compose logs -ft > /srv/server-logging/docker-compose/$(date +%s).log &
