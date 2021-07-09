e00db365e5d1a404a5443bbfbdc431fb42fb4a2d
unknownpgr <unknownpgr@gmail.com>
2021-07-09 07:22:20 +0000

    Add logging

--- reload.sh
+++ reload.sh
@@ -1,3 +1,5 @@
 docker-compose down --remove-orphans
 docker-compose up -d --build
 docker container ls
+mkdir /srv/server-logging/docker-compose
+docker-compose logs -ft > /srv/server-logging/docker-compose/$(date +%s).log &
