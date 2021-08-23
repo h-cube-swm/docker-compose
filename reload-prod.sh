2f28d5808ac8a0c7b3a9996ac71fb5aa5c3549a2
unknownpgr <unknownpgr@gmail.com>
2021-08-23 13:36:36 +0000

    Fix minor error

--- reload-prod.sh
+++ reload-prod.sh
@@ -3,4 +3,4 @@ docker-compose -f docker-compose.prod.yml down --remove-orphans
 docker-compose -f docker-compose.prod.yml up -d --build
 docker container ls
 mkdir /srv/server-logging/docker-compose 2> /dev/null
-docker-compose logs -ft > /srv/server-logging/docker-compose/$(date +%s).log &
+docker-compose -f docker-compose.prod.yml logs -ft > /srv/server-logging/docker-compose/$(date +%s).log &
--- restart-backend.sh
+++
@@ -1,3 +0,0 @@
-cd "$(dirname "$0")"
-docker-compose up -d --build gunicorn-backend
-docker-compose logs -tf gunicorn-backend
