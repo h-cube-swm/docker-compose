3616b25ee21a9615f6c77c1dea68fc52fc426fee
unknownpgr <unknownpgr@gmail.com>
2021-08-23 13:28:15 +0000

    Split docker-compose.yml into dev and prod

--- reload-prod.sh
+++ reload-prod.sh
@@ -1,6 +1,6 @@
 cd "$(dirname "$0")"
-docker-compose down --remove-orphans
-docker-compose up -d --build
+docker-compose -f docker-compose.prod.yml down --remove-orphans
+docker-compose -f docker-compose.prod.yml up -d --build
 docker container ls
 mkdir /srv/server-logging/docker-compose 2> /dev/null
 docker-compose logs -ft > /srv/server-logging/docker-compose/$(date +%s).log &
