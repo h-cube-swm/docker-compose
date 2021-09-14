0ca8982234a02b9fd80ea8c2b4f3d9dfabb258cc
unknownpgr <unknownpgr@gmail.com>
2021-09-14 10:42:00 +0000

    Remove production setting

--- reload-dev.sh
+++ reload-dev.sh
@@ -1,5 +1,5 @@
 cd "$(dirname "$0")"
-docker-compose -f docker-compose.dev.yml down --remove-orphans
-docker-compose -f docker-compose.dev.yml up -d --build
+docker-compose down --remove-orphans
+docker-compose up -d --build
 docker container ls
 mkdir /srv/server-logging/docker-compose 2> /dev/null
--- reload-prod.sh
+++
@@ -1,6 +0,0 @@
-cd "$(dirname "$0")"
-docker-compose -f docker-compose.prod.yml down --remove-orphans
-docker-compose -f docker-compose.prod.yml up -d --build
-docker container ls
-mkdir /srv/server-logging/docker-compose 2> /dev/null
-docker-compose -f docker-compose.prod.yml logs -ft > /srv/server-logging/docker-compose/$(date +%s).log &
